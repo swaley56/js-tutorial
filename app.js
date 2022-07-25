20db274ad8420d406c11f28011ad93bb30c6bf67
Sobur <57179205+swaley56@users.noreply.github.com>
2022-07-25 16:44:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ class Developer {
 
 let mybio = new Developer('Subair')
 console.log(mybio.name)
+
+
+class DataDesigner {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+let mybio = new DataDesigner('Sobur')
+console.log(mybio.name)
